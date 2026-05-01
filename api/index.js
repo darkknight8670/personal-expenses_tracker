d24baa54bbac20b0bc3d1181f430aca046fc8006
Vikas Barupal <162597481+darkknight8670@users.noreply.github.com>
2026-05-01 23:03:26 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,3 @@
-const serverless = require('serverless-http');
-const { app } = require('../server');
-
-module.exports = serverless(app);
+export default function handler(req, res) {
+  res.status(200).send("Backend is working 🚀");
+}
